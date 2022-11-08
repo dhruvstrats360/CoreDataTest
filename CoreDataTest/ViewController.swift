d90d09b58e97b9e879c8d9d162deabc2a08ad8f5
dhruvstrats360 <dhruvchhatbar@strats360.com>
2022-11-08 18:05:43 +0530

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Nirav Desai on 08/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
